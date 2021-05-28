cbf4aadd65fc3e307a43fc465fbc46e6d1c5e74f
chengqiujing <wowcheng163@163.com>
2021-05-28 16:41:46 +0800

    kafka简单使用测试

---
+++ src/main/java/com/test/demo/kafka/producer/ProducerTest.java
@@ -0,0 +1,16 @@
+package com.test.demo.kafka.producer;
+
+/**
+ * *************************************************************************
+ * <p/>
+ *
+ * @文件名称: ProducerTest.java
+ * @包 路 径： com.test.demo.kafka.producer
+ * @版权所有：北京数字认证股份有限公司 (C) 2021
+ * @类描述:
+ * @版本: V1.0
+ * @author：chengqj
+ * @创建时间：2021/5/28 16:40
+ */
+public class ProducerTest {
+}
