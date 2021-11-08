1fade71ddd619f8eb4ebcc27123f4861e6c1ae95
chengqj <wowcheng163@163.com>
2021-11-08 10:09:07 +0800

    验证定时任务

--- src/main/java/com/test/demo/basic/ScheduleTest.java
+++ src/main/java/com/test/demo/basic/ScheduleTest.java
@@ -3,10 +3,7 @@ package com.test.demo.basic;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 
@@ -15,40 +12,53 @@ public class ScheduleTest {
         // spring
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();;
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(1);
+        threadPoolTaskScheduler.initialize();
+        Calendar instance = Calendar.getInstance();
+        instance.set(2021,8,13,23,55);
+        threadPoolTaskScheduler.schedule(() -> {
+            System.out.println(123);
+        }, instance.getTime());
+
+        try {
+            TimeUnit.SECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // spring
 
 
-        ScheduledExecutorService service = new ScheduledThreadPoolExecutor(1);
-        service.schedule(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("111");
-            }
-        },3000,TimeUnit.MILLISECONDS);
-//        service.scheduleAtFixedRate()
-
-        Timer timer = new Timer();
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        System.out.println("1ci");
-                    }
-                }, 3000, 1000);
-            }
-        });
-        thread.setDaemon(false);
+//        ScheduledExecutorService service = new ScheduledThreadPoolExecutor(1);
+//        service.schedule(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("111");
+//            }
+//        },3000,TimeUnit.MILLISECONDS);
+////        service.scheduleAtFixedRate()
+//
+//        Timer timer = new Timer();
+//
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                timer.schedule(new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        System.out.println("1ci");
+//                    }
+//                }, 3000, 1000);
+//            }
+//        });
+//        thread.setDaemon(false);
 //        thread.start();
 
-//        try {
-//            TimeUnit.SECONDS.sleep(10);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 //        timer.cancel();
 //
 //        try {
