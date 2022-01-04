9ffa53e4cdcbfaee8fd2df439abbd0745707cb4e
chengqj <wowcheng163@163.com>
2022-01-04 16:11:01 +0800

    测试：p10解析

--- src/main/java/com/test/demo/encryp/P10parse.java
+++ src/main/java/com/test/demo/encryp/P10parse.java
@@ -35,7 +35,8 @@ public class P10parse {
         System.out.println(s);
 
 
-        String sb = "MIHnMIGMAgEAMCoxCzAJBgNVBAYMAkNOMRswGQYDVQQDDBIwMC0wQy0yOS1EOC0yNC1FRjswWTATBgcqhkjOPQIBBggqgRzPVQGCLQNCAARR3wIB2kRnfMBVHy87jrIyikTjBm31VT4V2vTWFvPbnkv77zmadmQMgTAyYqbFJUrDa3rAho/SsFpbBMRXD9IyoAAwDAYIKoEcz1UBg3UFAANIADBFAiAQOAATHH5Wwz4GTG9kfT4CuPZZWrRdWL3DRNW+03ztEQIhAIsxw1HGiXWF/FLyHudOvKYFt8hiLqZnRaHv4d7NE8Z4";//这里传入P10请求字符串，注意去掉换行符
+        String sb = "MIIBCzCBsAIBADBOMS0wKwYDVQQDDCQwMTAwYzA1Ni0wMDIxLTAwMmMtMDEwMi0wMzA0MDUwNjA3MDgxEDAOBgNVBAgMB0JlaWppbmcxCzAJBgNVBAYMAkNOMFkwEwYHKoZIzj0CAQYIKoEcz1UBgi0DQgAEvy8NOWg9nx8rb9fQqaujzW3hzqwE9n37t37FaceGnQc3O2NPsu25dArSRhU5gtvJn2qVgg1pn8XG7oQ77QEyJKAAMAwGCCqBHM9VAYN1BQADSAAwRQIgUZStmpURUDtg4BH5aQtZ/SW5eVLpCfTSgrnF/h+7kkkCIQDr4P5AfDTtD/ccNIxu91W20LattMN6ksI8pjgdUJIIyw==";//这里传入P10请求字符串，注意去掉换行符
+        // 国密p10对象
         PKCS10CertificationRequest request = new PKCS10CertificationRequest(Base64.getDecoder().decode(sb));
         System.out.println(request.toString());
         ASN1Set attributes = request.getCertificationRequestInfo().getAttributes();
@@ -44,6 +45,26 @@ public class P10parse {
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * 解析p10
+     * @throws NoSuchAlgorithmException
+     * @throws SignatureException
+     * @throws IOException
+     */
     public static void parsep10() throws NoSuchAlgorithmException, SignatureException, IOException {
         String sb = "MIHnMIGMAgEAMCoxCzAJBgNVBAYMAkNOMRswGQYDVQQDDBIwMC0wQy0yOS1EOC0yNC1FRjswWTATBgcqhkjOPQIBBggqgRzPVQGCLQNCAARR3wIB2kRnfMBVHy87jrIyikTjBm31VT4V2vTWFvPbnkv77zmadmQMgTAyYqbFJUrDa3rAho/SsFpbBMRXD9IyoAAwDAYIKoEcz1UBg3UFAANIADBFAiAQOAATHH5Wwz4GTG9kfT4CuPZZWrRdWL3DRNW+03ztEQIhAIsxw1HGiXWF/FLyHudOvKYFt8hiLqZnRaHv4d7NE8Z4";//这里传入P10请求字符串，注意去掉换行符
         PKCS10 pkcs10 = new PKCS10(Base64.getDecoder().decode(sb));//解析成P10对象
@@ -57,6 +78,14 @@ public class P10parse {
         System.out.println(publicKey.getAlgorithm());
     }
 
+    /**
+     * 获取私钥
+     * @return
+     * @throws InvalidKeyException
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchProviderException
+     * @throws SignatureException
+     */
     public static String genCSR()
             throws InvalidKeyException, NoSuchAlgorithmException,
             NoSuchProviderException, SignatureException {
