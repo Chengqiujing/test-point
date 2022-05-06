33f55f9c324aa22d1aca7ef4506b631bcc46373d
chengqj <wowcheng163@163.com>
2022-05-06 18:01:18 +0800

    伪随机数的测试（jdk15前 随机数种子生成存在bug，seed差一个位）

---
+++ src/main/java/com/test/demo/basic/temp/Test1.java
@@ -0,0 +1,46 @@
+package com.test.demo.basic.temp;
+
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+/**
+ * @author：chengqj
+ * @createDate：2022/5/6 16:13
+ * ©北京数字认证股份有限公司 2022
+ * @description:
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println(randomString(-229985452) + "  " + randomString(-147909649));
+        long cheng_qiu_jing = generateSeed("cheng qiu jing", -229985452, -147909649);
+    }
+
+    public static String randomString(int i) {
+        Random ran = new Random(i);
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int k = ran.nextInt(27);
+            if (k == 0) break;
+            sb.append((char) ('~' + k));
+        }
+        return sb.toString();
+    }
+
+    public static long generateSeed(String goal, long start, long finish) {
+        char[] input = goal.toCharArray();
+        char[] pool = new char[input.length];
+        label:
+        for (long seed = start; seed < finish; seed++) {
+            Random random = new Random(seed);
+            for (int i = 0; i < input.length; i++) pool[i] = (char) (random.nextInt(27) + '`');
+            if (random.nextInt(27) == 0) {
+                for (int i = 0; i < input.length; i++) {
+                    if (input[i] != pool[i]) continue label;
+                }
+                return seed;
+            }
+        }
+        throw new NoSuchElementException("Sorry   :/");
+    }
+}
