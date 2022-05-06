d29462d580b6eccac61674a5f2ac9472ec75b1ac
chengqj <wowcheng163@163.com>
2022-05-06 15:51:33 +0800

    验证spring 定时任务超过时间间隔是如何执行的，单线程执行，如果想要多线程，需要配置线程池

--- src/main/java/com/test/demo/web/SpringSchedule.java
+++ src/main/java/com/test/demo/web/SpringSchedule.java
@@ -1,5 +1,7 @@
 package com.test.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.scheduling.support.CronTrigger;
@@ -13,6 +15,9 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SpringSchedule implements SchedulingConfigurer {
+
+    @Autowired
+    RedisTemplate redisTemplate;
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         taskRegistrar.addTriggerTask(
@@ -21,11 +26,29 @@ public class SpringSchedule implements SchedulingConfigurer {
                     验证一下报错是否还执行
                     结果：报错是可以执行得
                      */
-                    int a = 1/0;
+//                    int a = 1/0;
+                    doProcess();
                 },
                 triggerContext -> {
-                    return new CronTrigger("0/1 * * * * ?").nextExecutionTime(triggerContext);
+                    return new CronTrigger("0/3 * * * * ?").nextExecutionTime(triggerContext);
                 }
         );
     }
+
+
+
+    public void doProcess(){
+        System.out.println("进来：" + Thread.currentThread().getName() + " t-id:" + Thread.currentThread().getId());
+
+
+        try {
+
+            Thread.sleep(1000000000);
+            System.out.println("finish!!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
